0a5b1d1480a1fea281ab751c9f4abe3b43978b86
inqware <ievinnik@gmail.com>
2023-06-16 16:12:02 +0300

    Here we go!

---
+++ test.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+type Question struct {
+	qOrder              int
+	qType               string
+	questionDescription string
+	rightAnswer         string
+	maxScore            int
+	numberOfAttempts    int
+	isTimed             bool
+	hasMedia            bool
+}
+
+func addNewQuestion(qOrder int, qType string, questionDescription string, rightAnswer string, maxScore int, numberOfAttempts int, isTimed bool, hasMedia bool) *Question {
+	q := Question{qOrder: qOrder}
+	q.questionDescription = questionDescription
+	q.qType = qType
+	q.maxScore = maxScore
+	q.rightAnswer = rightAnswer
+	q.numberOfAttempts = numberOfAttempts
+	q.isTimed = isTimed
+	q.hasMedia = hasMedia
+	fmt.Println("Successfully added")
+	return &q
+}
+func main() {
+	//var quizName = "My first quiz"
+	//var numberOfQuestions = 2
+	//timed := false
+	var qu1 = addNewQuestion(1, "multipleChoice", "Number of planets in Solar system", "8", 10, 2, false, false)
+
+	fmt.Println("Question number is:", qu1.qOrder)
+}
