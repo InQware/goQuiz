b54417912efd3275904d15d7d9c9010849fb0dbd
InQware <34445366+InQware@users.noreply.github.com>
2023-07-03 17:21:48 +0300

    Added functions for counting and comparing questions

--- test.go
+++ test.go
@@ -2,12 +2,6 @@ package main
 
 import (
 	"fmt"
-
-	"fyne.io/fyne/v2"
-	"fyne.io/fyne/v2/app"
-	"fyne.io/fyne/v2/container"
-	"fyne.io/fyne/v2/layout"
-	"fyne.io/fyne/v2/widget"
 )
 
 type Question struct {
@@ -37,29 +31,49 @@ func addNewQuestion(qOrder int, qType string, questionDescription string, rightA
 	return &q
 }
 
+func checkForDoubles(q1, q2 Question) bool {
+	var isDouble bool
+	if q1 == q2 {
+		isDouble = true
+	} else {
+		isDouble = false
+	}
+	return isDouble
+}
+
+func countAddedQuestions(questList []*Question) int {
+	numQ := len(questList)
+	return numQ
+}
+
 func main() {
 	var questionsArray []*Question
-	qSum := 0
-	myApp := app.New()
-	myWindow := myApp.NewWindow("Questions")
+	//qSum := 0
+	//myApp := app.New()
+	//myWindow := myApp.NewWindow("Questions")
 	//myCanvas := myWindow.Canvas()
 	//grey := color.NRGBA{R: 200, G: 200, B: 200, A: 255}
 	//rect := canvas.NewRectangle(grey)
 	//Make add button
-	addBtn := widget.NewButton("Add question", func() {
-		addNewQuestion(1, "multipleChoice", "Number of planets in Solar system", "8", 10, 2, false, false)
-	})
+	/*addBtn := widget.NewButton("Add question", func() {
+		//var voidance struct
+		//qSum, voidance  = addNewQuestion(qSum, 1, "multipleChoice", "Number of planets in Solar system", "8", 10, 2, false, false)
+		headText := "Type your question here"
+		hTxt := canvas.NewText(headText, color.Gray)
+		qForm := widget.NewEntry()
+		newQuestion := container.New(layout.NewBoxLayout(), hTxt, layout.NewSpacer(), qForm)
+	})*/
 	//Sum all the questions:
-	qSumStr := fmt.Sprintln("Questions added:", qSum)
+	//qSumStr := fmt.Sprintln("Questions added:", qSum)
 	//content := widget.NewLabel(qSumStr)
 	//content := canvas.NewText("1", color.White)
-	content := widget.NewLabel(qSumStr)
+	////content := widget.NewLabel(qSumStr)
 	//myCanvas.SetContent(rect)
-	grid := container.New(layout.NewGridLayout(2), content, addBtn)
-	myWindow.Resize(fyne.NewSize(800, 600))
-	myWindow.SetContent(grid)
+	////grid := container.New(layout.NewGridLayout(2), content, addBtn)
+	////myWindow.Resize(fyne.NewSize(800, 600))
+	////myWindow.SetContent(grid)
 	//myWindow.SetContent(addBtn)
-	myWindow.ShowAndRun()
+	////myWindow.ShowAndRun()
 
 	// 	widget.NewButton("Hi!", func() {
 	// 		hello.SetText("Welcome :)")
@@ -69,6 +83,18 @@ func main() {
 	//var quizName = "My first quiz"
 	//var numberOfQuestions = 2
 	//timed := false
-	var newQ = addNewQuestion(1, "multipleChoice", "Number of planets in Solar system", "8", 10, 2, false, false)
+	var newQ *Question
+	newQ = addNewQuestion(1, "multipleChoice", "Number of planets in Solar system", "8", 10, 2, false, false)
 	questionsArray = append(questionsArray, newQ)
+	fmt.Println("Total number of questions:")
+	questionsArray = append(questionsArray, newQ)
+	//time.Sleep(2 * time.Second)
+	numQ := countAddedQuestions(questionsArray)
+	fmt.Println(numQ)
+	isDoubleQuestion := checkForDoubles(*questionsArray[numQ-1], *questionsArray[numQ-2])
+	if isDoubleQuestion {
+		fmt.Println("Last 2 questions are doubles")
+	} else {
+		fmt.Println("Unique questions, proceeding")
+	}
 }
